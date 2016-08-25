44681a0a0146903b8ad814a05369d9dfa5cf7bcd
Cris Ward <cris@duodesign.co.uk>
2016-08-25 19:03:35 +0100

    refactoring

---
+++ src/app/room.cr
@@ -0,0 +1,22 @@
+class Room
+
+  def initialize
+    @sockets = [] of HTTP::WebSocket
+  end
+
+  def add(socket : HTTP::WebSocket)
+    @sockets << socket
+    p "connected",@sockets.size
+  end
+
+  def remove(socket : HTTP::WebSocket)
+    @sockets.delete socket
+  end
+
+  def send_all(json : String)
+    @sockets.each do |socket| 
+      socket.send json
+    end
+  end
+
+end
